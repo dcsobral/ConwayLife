fcdb90342d1975c2327abe660da421f813a42d51
Daniel Sobral <dcsobral@gmail.com>
2011-05-22 02:29:23 -0300

    Avoid using versions that should come from SBT itself.

--- project/build/ConwayLifeProject.scala
+++ project/build/ConwayLifeProject.scala
@@ -3,7 +3,7 @@ import sbt._
 class ConwayLifeProject(info: ProjectInfo) extends DefaultProject(info) {
   override def compileOptions = Deprecation :: Unchecked :: ExplainTypes :: super.compileOptions.toList
   val snapshots = ScalaToolsSnapshots 
-  val sc = "org.scala-tools.testing" % "scalacheck_2.8.0.Beta1-RC5" % "1.7-SNAPSHOT"
-  val scalaSwing = "org.scala-lang" % "scala-swing" % "2.8.0.Beta1-RC7"
+  val sc = "org.scala-tools.testing" %% "scalacheck" % "1.9"
+  val scalaSwing = "org.scala-lang" % "scala-swing"
   val mavenLocal = "Local Maven Repository" at "file://"+Path.userHome+"/.m2/repository"
 }
